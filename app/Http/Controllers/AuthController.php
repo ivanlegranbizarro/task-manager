cf870999c8ef766a9589dce9b24b570d5615f650
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-09-08 20:48:40 +0200

    signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function signup(SignupRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $validated['password'] = bcrypt($validated['password']);
+
+        $user = User::create($validated);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
+
     public function login(LoginRequest $request): JsonResponse
     {
         $validated = $request->validated();
@@ -25,5 +44,13 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
+    }
+
+
+    public function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Successfully logged out'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('signup', [AuthController::class, 'signup'])->name('signup');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/user', function (Request $request) {
